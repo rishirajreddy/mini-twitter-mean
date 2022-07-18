f02d00fb111c214b5f5ca190ec55493583067eb8
rishirajreddy <rishirajreddy.07@gmail.com>
2022-07-18 21:31:39 +0530

    modified .env

--- db/db.js
+++ db/db.js
@@ -2,10 +2,9 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config();
-const url = "mongodb+srv://mrrobot:mrrobot@empmngmt.cs734kp.mongodb.net/test-2?retryWrites=true&w=majority";
 
 const connect = () => {
-    mongoose.connect(url, {
+    mongoose.connect(process.env.MONGO_URL, {
         useNewUrlParser : true, useUnifiedTopology: true
     });
     console.log("Connected to database!!");
